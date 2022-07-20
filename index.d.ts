c562dbf2a31aed1cc272a1b3659e87a985fd5145
Matt Karl <matt@mattkarl.com>
2022-07-20 13:18:00 -0400

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Convert evenodd fill-rule to support nonzero fill-rule.
+ * @param params Buffer of the SVG  
+ * @return Output buffer of the SVG
+ */
+export declare function reorient (params:string): string;
